e089d677590b2bfbf2f4651d97fe0aea8156fefc
JR2405 <juniorpaulino2324@gmail.com>
2026-03-14 23:40:57 -0400

    fix sqlite path

--- backend/database.js
+++ backend/database.js
@@ -1,8 +1,14 @@
 const Database = require('better-sqlite3');
 const path = require('path');
+const fs = require('fs');
 const bcrypt = require('bcryptjs');
 
-const DB_PATH = path.join(__dirname, '../database/database.sqlite');
+const DB_DIR = path.join(__dirname, '../database');
+const DB_PATH = path.join(DB_DIR, 'database.sqlite');
+
+if (!fs.existsSync(DB_DIR)) {
+  fs.mkdirSync(DB_DIR, { recursive: true });
+}
 
 let db;
 
